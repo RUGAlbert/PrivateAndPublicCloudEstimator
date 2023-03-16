d345b95b5a5499a0b07f007a180d2f86ed147e3b
albert dijkstra <albertdijkstra1998@hotmail.com>
2023-03-16 09:38:02 +0100

    started with a linear regression model

--- westerhof1/calculator/scope2/eServer.py
+++ westerhof1/calculator/scope2/eServer.py
@@ -5,9 +5,21 @@ This calculates the server energy
 
 from numpy import NaN
 from pandas import DataFrame
+from sklearn import linear_model
+
+def calculateCoefficients(hourlyDf: DataFrame):
+    regr = linear_model.LinearRegression()
+    regr.fit(hourlyDf[['amount_CPU_CLK_UNHALTED', 'cache_data_moved', 'amount_DRAM_ACCESSES', 'disk_data_moved']], hourlyDf['power_consumed'])
 
 
 def calculate(hourlyDf : DataFrame):
+
+
+    #prepare dataframe
+    hourlyDf['cache_data_moved'] = hourlyDf['amount_INSTRUCTION_CACHE_FETCHES'] +hourlyDf['amount_DATA_CACHE_FETCHES']
+    hourlyDf['disk_data_moved'] = hourlyDf['disk_bytes_read'] + hourlyDf['disk_bytes_written']
+
+
     
     # https://stackoverflow.com/questions/54754880/linear-regression-over-two-variables-in-a-pandas-dataframe
     # Do this later
@@ -18,11 +30,12 @@ def calculate(hourlyDf : DataFrame):
     cacheDataMoved = 0
     dramAccessed = 1.828e-4
     diskDataMoved = 1.365e-6
+    
 
     eServer = (idlePower + cpuUtil * hourlyDf['amount_CPU_CLK_UNHALTED'] + 
-        cacheDataMoved * (hourlyDf['amount_INSTRUCTION_CACHE_FETCHES'] +hourlyDf['amount_DATA_CACHE_FETCHES']) +
+        cacheDataMoved * hourlyDf['cache_data_moved'] +
         dramAccessed * hourlyDf['amount_DRAM_ACCESSES'] +
-        diskDataMoved * (hourlyDf['disk_bytes_read'] + hourlyDf['disk_bytes_written'])
+        diskDataMoved * hourlyDf['disk_data_moved']
     )
 
     return eServer
